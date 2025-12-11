8a5bd0bbfeda6af4871c356fe50a18a5394f51b4
fazamumtaz <fazamumtazramadhan@gmail.com>
2025-12-11 22:09:57 +0700

    feat: login

---
+++ src/app/(auth)/login/page.js
@@ -0,0 +1,88 @@
+import Link from "next/link";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <main className="min-h-screen lg:h-screen w-full flex flex-col lg:flex-row bg-gradient-to-br from-[#05121b] to-[#0D1922]">
+      {/* Left Section */}
+      <div className="w-full lg:w-2/5 xl:w-1/2 flex items-center justify-center p-6 lg:p-10">
+        <div className="w-full max-w-md space-y-8">
+          {/* Logo */}
+          <img src="/logo.svg" alt="Logo" className="w-10" />
+
+          {/* Heading */}
+          <div className="space-y-3">
+            <h1 className="font-bold text-4xl lg:text-4xl leading-tight text-white">
+              Mulai perjalanan menjelajahi kekayaan budaya nusantara.
+            </h1>
+            <p className="text-gray-400 text-base lg:text-lg">
+              Temukan keindahan dan kearifan lokal Indonesia
+            </p>
+          </div>
+
+          {/* Google OAuth Button */}
+          <Link href="/login mb-3">
+            <button className="w-full bg-white border-2 border-gray-200 hover:border-gray-300 hover:bg-gray-200 transition-all duration-300 rounded-xl px-6 py-2.5 flex items-center justify-center gap-3 text-gray-700 font-semibold text-base group">
+              <svg
+                className="w-5 h-5 group-hover:scale-110 transition-transform"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#4285F4"
+                  d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                />
+                <path
+                  fill="#34A853"
+                  d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                />
+                <path
+                  fill="#FBBC05"
+                  d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                />
+                <path
+                  fill="#EA4335"
+                  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                />
+              </svg>
+              <span>Continue with Google</span>
+            </button>
+          </Link>
+
+          {/* Additional Info */}
+          <p className="text-xs text-gray-500 text-left mt-5">
+            Dengan masuk, Anda menyetujui{" "}
+            <a href="#" className="text-(--color-secondary) hover:underline">
+              Syarat & Ketentuan
+            </a>{" "}
+            dan{" "}
+            <a href="#" className="text-(--color-secondary) hover:underline">
+              Kebijakan Privasi
+            </a>
+          </p>
+        </div>
+      </div>
+
+      {/* Right Section - Image */}
+      <div className="w-full lg:w-3/5 xl:w-1/2 p-3 lg:p-4">
+        <div className="relative h-64 lg:h-full w-full rounded-2xl overflow-hidden shadow-2xl group">
+          <img
+            src="/candi.jpg"
+            alt="Candi Indonesia"
+            className="h-full w-full object-cover object-center transition-transform duration-700 group-hover:scale-105"
+          />
+          {/* Gradient Overlay */}
+          <div className="absolute inset-0 bg-linear-to-t from-black/30 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
+
+          {/* Optional: Floating Badge */}
+          <div className="absolute lg:bottom-6 lg:left-6 left-1 bottom-1 lg:bg-white/10 bg-transparent lg:backdrop-blur-sm px-3 py-1 rounded-full shadow-lg">
+            <p className="lg:text-sm text-xs font-medium italic text-white">
+              Candi Borobudur, Kabupaten Magelang, Jawa Tengah
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default LoginPage;
